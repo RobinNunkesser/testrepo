c99179e336313774880eaa1caab7a1b10e708e59
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-06-04 15:17:52 +0200

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Prof. Dr. Nunkesser, Robin on 04.06.19.
+//  Copyright © 2019 Hochschule Hamm-Lippstadt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
